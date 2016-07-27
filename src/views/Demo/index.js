f872a30ce626ff441a5152ca666d62619f3f9178
bert <429874664@qq.com>
2016-07-27 22:35:25 +0800

    学习单元测试

---
+++ src/views/Demo/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+class Root extends Component {
+  render() {
+    return <h1> Hello World </h1>;
+  }
+}
+
+export default Root;
